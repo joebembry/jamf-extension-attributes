583b5de513df327d87229315b02bdb05ebc3f82f
joebembry <jbembry@monmouth.edu>
2023-04-19 19:42:09 -0400

    Create sasafras-keyaccess.sh

---
+++ Sassafras Software/sassafras-keyaccess.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#####################################################################
+# A script to collect the Bundle Version of Alertus Desktop Client. #
+#####################################################################
+
+PLIST="/Library/KeyAccess/KeyAccess.app/Contents/Info.plist"
+KEY="CFBundleShortVersionString"
+if [ -f "${PLIST}" ]; then
+	RESULT=$(/usr/bin/defaults read "${PLIST}" "${KEY}" 2>/dev/null)
+else
+	RESULT="Not Installed"
+fi
+
+/bin/echo "<result>${RESULT}</result>"
+
+exit 0
